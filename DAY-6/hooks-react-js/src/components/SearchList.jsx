4cdee48b9607c54f1bb4b0affefa1ee647b6df2e
deepakkumarshinde1 <deepakkumar.shinde1@gmail.com>
2025-10-14 10:44:16 +0530

    DAY-6 Other Hooks

--- DAY-6/hooks-react-js/src/components/SearchList.jsx
+++ DAY-6/hooks-react-js/src/components/SearchList.jsx
@@ -1,44 +1,46 @@
-import React, { useEffect, useState, useTransition } from "react";
+import React, {
+  useDeferredValue,
+  useEffect,
+  useMemo,
+  useState,
+  useTransition,
+} from "react";
 
 function SearchList() {
   let [list, setList] = useState([]);
-  let [filterList, setFilterList] = useState([]);
-  let [input, setInput] = useState("");
-  let [isPending, startTransition] = useTransition();
 
-  let filterResult = (event) => {
+  let [input, setInput] = useState("");
+  let differValue = useDeferredValue(input);
+
+  let handleInput = (event) => {
     let { value } = event.target;
     setInput(value);
-    console.log("start");
-    startTransition(() => {
-      let newList = list.filter((product) => {
-        console.log("loop");
-        return product.title.toLowerCase().includes(value.toLowerCase());
-      });
-      setFilterList(newList);
-    });
-    console.log("top");
   };
+
+  let filterList = useMemo(() => {
+    return list.filter((product) => {
+      return product.title.toLowerCase().includes(differValue.toLowerCase());
+    });
+  }, [differValue]);
+
   useEffect(() => {
     (async () => {
       let response = await fetch(`https://fakestoreapi.com/products`);
       let data = await response.json();
       setList(data);
-      setFilterList(data);
     })();
   }, []);
-
+  console.log(differValue);
   return (
     <>
       <p>Total project are {list.length}</p>
       <input
         value={input}
-        onChange={filterResult}
+        onChange={handleInput}
         type="text"
         placeholder="enter project name"
       />
       <ul>
-        {isPending && <p>Loading...</p>}
         {filterList.map((products, index) => {
           return <li key={index}>{products.title}</li>;
         })}
